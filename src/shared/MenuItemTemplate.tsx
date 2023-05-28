e145770b795b26f0d8e89c0eaad27951debbc762
Eduardodac <eduardo.dac@outlook.com>
2023-05-28 02:55:03 -0600

    Pequeña correción

--- src/shared/MenuItemTemplate.tsx
+++ src/shared/MenuItemTemplate.tsx
@@ -1,14 +1,12 @@
 import { NavLink } from 'react-router-dom'
 
 interface ItemProps {
-    icon: string,
     label: string,
     path: string,
-    img?: string
+    img: string
 }
 
 export const MenuItemTemplate = ({
-    icon = 'pi pi home',
     label = 'Home',
     path = '',
     img,
@@ -23,9 +21,7 @@ export const MenuItemTemplate = ({
                 to={`/${path}`}
             >
                 <div className='flex items-center gap-2 text-sm py-2 pl-3'>
-                    {img != null
-                        ? (<img src={img} alt={path} className='h-5 w-5 opacity-60' />)
-                        : (<i className={`${icon}`}></i>)}
+                <img src={img} alt={path} className='h-5 w-5 opacity-60' />
                     <p>{label}</p>
                 </div>
             </NavLink>
