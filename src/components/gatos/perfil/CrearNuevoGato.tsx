e9b032a50077d9edc7eabcb8c54304b97084e344
Eduardodac <eduardo.dac@outlook.com>
2024-05-18 00:02:22 -0600

    Agrega boton nuevo gato

--- src/components/gatos/perfil/CrearNuevoGato.tsx
+++ src/components/gatos/perfil/CrearNuevoGato.tsx
@@ -1,6 +1,22 @@
+import { motion } from "framer-motion";
+import plus from "../../../assets/plus.svg";
 
 export const CrearNuevoGato = () => {
-  return (
-    <div>CrearNuevoGato</div>
-  )
+    return (
+
+        <article className="mt-16 w-full ">
+            <motion.div
+                className="rounded-full h-14 bg-fondoCards w-56 p-3 m-auto flex flex-row items-center card-cats"
+                initial={false}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                transition={{ duration: 0.2 }}
+            >
+                <div className="bg-fondo rounded-full text-fondoCards w-fit p-1">
+                    <img src={plus} alt="plus" width={30}></img>
+                </div>
+                <div className="ml-3 text-xl font-extrabold text-fondo">Nuevo Perfil</div>
+            </motion.div>
+        </article>
+    )
 }
--- src/pages/gatos/Gatos.tsx
+++ src/pages/gatos/Gatos.tsx
@@ -1,10 +1,12 @@
 import { CardGato } from "../../components/gatos/cards/CardGato"
+import { CrearNuevoGato } from "../../components/gatos/perfil/CrearNuevoGato"
 
 export const Gatos = () => {
   return (
     <article>
         <section className="text-2xl font-bold text-paletaIpn-guinda self-start mb-5">Perfil de tus mascotas</section>
         <CardGato/>
+        <CrearNuevoGato/>
     </article>
   )
 }
