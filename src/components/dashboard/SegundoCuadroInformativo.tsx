a75fb0693cb70cd225a947265a3854c60e08e9bc
Eduardodac <eduardo.dac@outlook.com>
2025-03-06 02:02:25 -0600

    Agrega opacidad iconos

--- src/components/dashboard/SegundoCuadroInformativo.tsx
+++ src/components/dashboard/SegundoCuadroInformativo.tsx
@@ -13,7 +13,7 @@ export const SegundoCuadroInformativo = ({text, icon, dia}:CuadroInformativoProp
                 <div className="w-full h-5/10 p-3 pt-5 text-center text-sm flex" >{text}</div>
                 <div className="w-full h-5/10 m-auto text-2xl text-center">{dia}</div>
             </div>
-            <img src={icon} width={50} height={50} className="m-auto"/>
+            <img src={icon} width={50} height={50} className="m-auto opacity-50"/>
         </div>
     </div>
   )
